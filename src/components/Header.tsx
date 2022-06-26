9b1fac6457dcefdd70caa0b51d1758cffaa95c72
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-06-26 16:21:22 +0200

    Add Profile Logo in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
-import headerStyle from '../styles/Header.module.scss';
+import HeaderStyle from '../styles/Header.module.scss';
 import Button from './Button';
 import { ReactComponent as Calendar } from '../assets/calendar.svg';
 import { ReactComponent as Phone } from '../assets/phone.svg';
 import headerImage from '../assets/header.png';
+import aroLogo from '../assets/aro_logo_3.svg';
 
 export interface HeaderProps { };
 
@@ -12,12 +13,14 @@ const Header: FC<HeaderProps> = (props) => {
 
     return (
         <>
-            <div className={headerStyle.titleWrapper}>
+            <div className={HeaderStyle.titleWrapper}>
                 <Button title='0177-1832931' icon={<Phone />} />
-                <h1 className={headerStyle.headerTitle}>I am the header</h1>
+                <div className={HeaderStyle.profileLogoWrapper}>
+                    <img src={aroLogo} alt="Aro Profile Logo" />
+                </div>
                 <Button title="Jetzt anfragen" border icon={<Calendar />} />
             </div>
-            <div className={headerStyle.headerImgWrapper}>
+            <div className={HeaderStyle.headerImgWrapper}>
                 <img src={headerImage} alt="logo" />
             </div>
         </>
