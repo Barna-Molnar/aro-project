3cdeb05787e047b870175fca5a395eef071762b9
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-06-26 20:42:42 +0200

    Creating Generic ComponentStyles.module.scss

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,29 @@
 import Divider from "./Divider";
-import AboutStyles from '../styles/About.module.scss';
+import ComponentStyles from '../styles/ComponentStyles.module.scss';
 import Button from "./Button";
 import { ReactComponent as Calendar } from '../assets/calendar.svg';
 
 const About = () => {
     return (
-        <div className={AboutStyles.wrapper}>
-
-            <div className={AboutStyles.titleWrapper}>
+        <div className={ComponentStyles.aboutContainer}>
+            <div className={ComponentStyles.leftTitleWrapper}>
+                <h2 className={ComponentStyles.title}>Warum aro?</h2>
                 <Divider />
-                <h2 className={AboutStyles.title}>Warum aro?</h2>
             </div>
-            <div className={AboutStyles.contentWrapper}>
-                <p>
-                    Sie möchten einen 9-Sitzer Bus oder einen Transporter mieten, mit oder ohne Fahrer, dann sind Sie hier richtig.
-                </p>
-                <p>
-                    Kundenzufriedenheit hat bei uns einen hohen Stellenwert. Deshalb bieten wir Qualität und Zuverlässigkeit zu fairen Preisen an.
-                </p>
-                <Button border title="Jetzt Anfragen" icon={<Calendar />} />
+            <div className={ComponentStyles.boxContentLeft}>
+                <div className={ComponentStyles.infoWrapper}>
+                    <div className={ComponentStyles.paragraphsWrapper}>
+                        <p>
+                            Sie möchten einen 9-Sitzer Bus oder einen Transporter mieten, mit oder ohne Fahrer, dann sind Sie hier richtig.
+                        </p>
+                        <p>
+                            Kundenzufriedenheit hat bei uns einen hohen Stellenwert. Deshalb bieten wir Qualität und Zuverlässigkeit zu fairen Preisen an.
+                        </p>
+                    </div>
+                    <div className={ComponentStyles.actionBtnWrapper}>
+                        <Button border title="Jetzt Anfragen" icon={<Calendar />} />
+                    </div>
+                </div>
             </div>
 
         </div>
--- src/components/Renting.tsx
+++ src/components/Renting.tsx
@@ -1,4 +1,4 @@
-import RentingStyles from '../styles/Renting.module.scss';
+import ComponentStyles from '../styles/ComponentStyles.module.scss';
 import Divider from './Divider';
 import renaultImage from '../assets/renault.png'
 import ControlledAccordions2 from './Accordion2';
@@ -9,30 +9,31 @@ import { ReactComponent as Galery } from '../assets/galery.svg';
 
 const Renting = () => {
     return (
-        <div className={RentingStyles.container}>
-            <div className={RentingStyles.titleWrapper}>
-                <h2 className={RentingStyles.title}>Vermietlung</h2>
+        <div className={ComponentStyles.container}>
+            <div className={ComponentStyles.rightTitleWrapper}>
+                <h2 className={ComponentStyles.title}>Vermietlung</h2>
                 <Divider />
             </div>
-            <div className={RentingStyles.contentBox}>
-                <div className={RentingStyles.imageWrapper}>
-                    <img src={renaultImage} alt="Reanult logo" />
-                </div>
-                <div className={RentingStyles.infoWrapper}>
-                    <p>
-                        Sie können bei uns einen Renault Trafic 9-Sitzer mit viel Platz und Beinfreiheit sowie großem Kofferraum mieten. Die hintere Reihe kann eingeklappt oder die Sitze rausgenommen werden.  Zudem können auch zwei Kindersitze und als auch eine Babyschale  zusätzlich angemietet werden. Auf Anfrage kann der Bus auch ausgeliefert werden.
-                    </p>
-                    <p>
-                        Der Bus ist Vollkasko versichert und kann von Personen  ab 23 Jahre angemietet werden. Die Kaution beträgt 350 €.
-                    </p>
+            <div className={ComponentStyles.boxContentRight}>
+                <div className={ComponentStyles.infoWrapper}>
+                    <div className={ComponentStyles.paragraphsWrapper}>
+                        <p>
+                            Sie können bei uns einen Renault Trafic 9-Sitzer mit viel Platz und Beinfreiheit sowie großem Kofferraum mieten. Die hintere Reihe kann eingeklappt oder die Sitze rausgenommen werden.  Zudem können auch zwei Kindersitze und als auch eine Babyschale  zusätzlich angemietet werden. Auf Anfrage kann der Bus auch ausgeliefert werden.
+                        </p>
+                        <p>
+                            Der Bus ist Vollkasko versichert und kann von Personen  ab 23 Jahre angemietet werden. Die Kaution beträgt 350 €.
+                        </p>
+                    </div>
                     <ControlledAccordions2 />
-                    <div className={RentingStyles.actionBtnWrapper}>
+                    <div className={ComponentStyles.actionBtnWrapper}>
                         <Button border title="Jetzt Anfragen" icon={<Calendar />} />
                         <Button border title="Zur Bildergalerie" icon={<Galery />} />
                     </div>
                 </div>
+                <div className={ComponentStyles.imageWrapper}>
+                    <img src={renaultImage} alt="Reanult logo" />
+                </div>
             </div>
-
         </div>
     )
 }
