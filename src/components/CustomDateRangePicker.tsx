2bd0e8564ba655bd062356e6c460294df9ac0b4d
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-07-03 21:05:35 +0200

    Find a way how to customize the Date range Component

--- src/components/CustomDateRangePicker.tsx
+++ src/components/CustomDateRangePicker.tsx
@@ -1,14 +1,12 @@
 // @ts-nocheck
 import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+// import 'react-date-range/dist/theme/default.css'; // theme css file
+import '../styles/ReactDateRangeThemeCustomization.scss'
 import { DateRange } from 'react-date-range';
 import { useState } from 'react';
 
 
-
 const CustomDatePicker = () => {
-
-
 
     const [state, setState] = useState([
         {
@@ -20,8 +18,11 @@ const CustomDatePicker = () => {
 
     console.log(state);
     return (
-        <div className="App">
+        <div >
             <DateRange
+
+                color='#AFF252'
+                // rangeColors={[`linear-gradient(286.72deg, #FFA420 0%, #FDD31D 100%)`]}
                 onChange={item => setState([item.selection])}
                 moveRangeOnFirstSelection={false}
                 ranges={state}
