749d84868b9c73de8251cb14d4e0436560983fa7
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-07-10 18:33:08 +0200

    define disabled borderBtn

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,12 +5,19 @@ export interface ButtonProps {
     title: string;
     icon: JSX.Element;
     border?: boolean;
+    disabled?: boolean;
+
 };
 
-const Button: FC<ButtonProps> = ({ icon, title, border, ...rest }) => {
+const Button: FC<ButtonProps> = ({ icon, title, border,disabled, ...rest }) => {
 
     return (
-        <button className={`${border ? ButtonStyle.borderButton : ButtonStyle.button}`} {...rest}>
+        <button className={`${border 
+        ? ButtonStyle.borderButton 
+        : ButtonStyle.button}
+         ${disabled ? ButtonStyle.disabledBorderBtn : ''}`} 
+        {...rest}
+        >
             {icon}
             {title}
         </button>
--- src/components/CustomDateRangePicker.tsx
+++ src/components/CustomDateRangePicker.tsx
@@ -31,6 +31,7 @@ const CustomDatePicker = () => {
                 color={null}
                 showMonthArrow={false}
             />
+            
         </div>
     );
 
