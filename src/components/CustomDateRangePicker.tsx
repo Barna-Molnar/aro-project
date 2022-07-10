f2dc84b65e0095a7595e5bdd33bcde4565707257
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2022-07-10 18:18:59 +0200

    Customized Calender

--- src/components/CustomDateRangePicker.tsx
+++ src/components/CustomDateRangePicker.tsx
@@ -4,25 +4,32 @@ import 'react-date-range/dist/styles.css'; // main style file
 import '../styles/ReactDateRangeThemeCustomization.scss'
 import { DateRange } from 'react-date-range';
 import { useState } from 'react';
+import { format } from 'date-fns'
 
+const formatDate = (date: string) => {
+    return format(new Date(date), 'MM/dd/yyyy')
+}
 
 const CustomDatePicker = () => {
 
     const [state, setState] = useState([
         {
             startDate: new Date(),
-            endDate: null,
-            key: "selection"
+            endDate: new Date(),
+            key: 'selection'
         }
     ]);
 
-    console.log(state);
+    console.log(formatDate(state[0].endDate));
+    console.log(formatDate(state[0].startDate));
     return (
         <div >
             <DateRange
                 onChange={item => setState([item.selection])}
                 moveRangeOnFirstSelection={false}
                 ranges={state}
+                color={null}
+                showMonthArrow={false}
             />
         </div>
     );
